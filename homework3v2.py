3f3dbf536bbac0a74b363c7974034f85aaca3286
angelina <80030972+angela104-maker@users.noreply.github.com>
2021-08-04 13:11:23 +0300

    Delete homework3v2.py

--- homework3v2.py
+++
@@ -1,8 +0,0 @@
-a = int(input("enter number: "))
-b = int(input("unter second number: "))
-if a<b:
-    for i in range(a, b+1):
-        print(i)
-else:
-    for i in range(a, b-1, -1):
-        print(i)

5eac5d7bc094b1e3a796ccf1ebdaee65c715e278
angelina <80030972+angela104-maker@users.noreply.github.com>
2021-08-04 13:11:12 +0300

    Delete homework3v1.py

--- homework3v1.py
+++
@@ -1,9 +0,0 @@
-a = 6
-for i in range(a):
-    for n in range(i):
-        print("*", end="")
-    print("")
-for i in range(a, 0, -1):
-    for n in range(i):
-        print("*", end="")
-    print("")
